0e584b3734ea96272bf36119eeb5c5e5ac0c3b4f
in0418gq <eugenecs326648@yahoo.com>
2015-10-05 14:17:40 -0500

    hashmap

--- src/com/cameoEugeneJessy/Cereal.java
+++ src/com/cameoEugeneJessy/Cereal.java
@@ -1,21 +1,23 @@
 package com.cameoEugeneJessy;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by in0418gq on 10/5/2015.
  */
 public class Cereal {
     private String name;
-    private  double rice;
-    private  double corn;
-    private  double sugar;
-    private  double salt;
-}
+    HashMap<String, Double> ingredients;
 
-public Cereal(String cerealName, double gramsRice, double gramsCorn, double gramsSugar, double gramsSalt){
+
+public Cereal(String cerealName, HashMap<String, Double> IngredientsList){
 
     this.name = cerealName;
-    this.corn = gramsCorn;
-    this.sugar = gramsSugar;
-    this.salt = gramsSalt;
-}
+    this.ingredients = IngredientsList;
+
+    }
+    public String getName() {return name;
+
+    }
 }
--- src/com/cameoEugeneJessy/Main.java
+++ src/com/cameoEugeneJessy/Main.java
@@ -1,8 +1,19 @@
 package com.cameoEugeneJessy;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner s = new Scanner(System.in);
+
+        for (Cereal c : allCereals){
+        System.out.println("How many boxes of " + c.getName() + " to order");
+        int boxes =  Integer.parseInt(s.nextLine());
     }
+
+    }
+
+
 }
