20e51a1f60dad1d5c320061da13c132f822b13bd
in0418gq <eugenecs326648@yahoo.com>
2015-10-05 13:19:55 -0500

    Initial commit

---
+++ src/com/cameoEugeneJessy/Cereal.java
@@ -0,0 +1,21 @@
+package com.cameoEugeneJessy;
+
+/**
+ * Created by in0418gq on 10/5/2015.
+ */
+public class Cereal {
+    private String name;
+    private  double rice;
+    private  double corn;
+    private  double sugar;
+    private  double salt;
+}
+
+public Cereal(String cerealName, double gramsRice, double gramsCorn, double gramsSugar, double gramsSalt){
+
+    this.name = cerealName;
+    this.corn = gramsCorn;
+    this.sugar = gramsSugar;
+    this.salt = gramsSalt;
+}
+}
---
+++ src/com/cameoEugeneJessy/Cerealmaster.java
@@ -0,0 +1,8 @@
+package com.cameoEugeneJessy;
+
+/**
+ * Created by in0418gq on 10/5/2015.
+ */
+public class Cerealmaster {
+    Cereal.add
+}
